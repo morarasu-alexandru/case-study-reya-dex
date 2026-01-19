92498052be20ed4c8d7c264e34af97b103dc79c2
Alex Morarasu <alexandru@helixic.io>
2026-01-19 22:07:07 +0200

    add satoshi font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 export default function Home() {
   return (
-    <div className="">
+    <div className="p-8">
+      <div className="font-bold">test bold</div>
+      <div className="font-medium">test medium</div>
     </div>
   );
 }
